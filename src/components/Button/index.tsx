bc09c7ee5edbff63f31b7a3a42c73b74ae95a97e
Felipe Augusto Rodrigues Nunes <felipe.augusto.rodrigues1@gmail.com>
2020-11-24 00:44:34 -0300

    feat: Add heading, media match and button components

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,22 @@
+import * as S from './styles'
+
+export type ButtonProps = {
+  children?: React.ReactNode
+  size?: 'small' | 'medium' | 'large'
+  fullWidth?: boolean
+  icon?: JSX.Element
+}
+
+const Button = ({
+  children,
+  size = 'medium',
+  fullWidth = false,
+  icon
+}: ButtonProps) => (
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon}>
+    {!!icon && icon}
+    {!!children && <span>{children}</span>}
+  </S.Wrapper>
+)
+
+export default Button
